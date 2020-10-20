f27673a51c82155acb29fa88b0c0eea864042954
healer1219 <lizewei1219@163.com>
2020-10-20 19:47:24 +0800

    new

--- cloud-provider-payment8001/src/main/java/com/healer/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/healer/springcloud/service/PaymentService.java
@@ -1,7 +1,6 @@
 package com.healer.springcloud.service;
 
 import com.healer.springcloud.entities.Payment;
-import org.apache.ibatis.annotations.Param;
 
 public interface PaymentService {
     int create(Payment payment);
