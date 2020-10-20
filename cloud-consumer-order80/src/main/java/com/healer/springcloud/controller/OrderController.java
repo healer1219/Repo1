d8c0a302818797489c9cf9047778e9b4c319f648
healer1219 <lizewei1219@163.com>
2020-10-20 17:11:54 +0800

    新的服务消费者order调用提供者payment

---
+++ cloud-consumer-order80/src/main/java/com/healer/springcloud/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.healer.springcloud.controller;
+
+import com.healer.springcloud.entities.CommonResult;
+import com.healer.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URL;
+
+@RestController
+@Slf4j
+public class OrderController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult findPaymentById(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
+    }
+
+}
